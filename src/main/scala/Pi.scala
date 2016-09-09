3ceea0a425f3329a5ca85c04c293cc7e88ed8df5
Lucas Bärenfänger <mail@scalarookie.com>
2016-09-09 18:58:54 +0200

    Add chapter "Creating the master"

--- src/main/scala/Pi.scala
+++ src/main/scala/Pi.scala
@@ -1,7 +1,7 @@
 import akka.actor._
 import akka.routing.RoundRobinRouter
 import akka.util.Duration
-import akka.util.duration
+import akka.util.duration._
 
 // The design we are aiming for is to have one `Master` actor initiating the computation, creating a set of `Worker`
 // actors. Then it splits up the work into discrete chunks, and sends these chunks to the different workers in a
@@ -10,13 +10,13 @@ import akka.util.duration
 // result.
 
 sealed trait PiMessage
-// `Calculate` -- sent to the `Master` actor to start the calculation
+// `Calculate` – sent to the `Master` actor to start the calculation
 case object Calculate extends PiMessage
-// `Work` -- sent from the `Master` actor to the `Worker` actors containing the work assignment
+// `Work` – sent from the `Master` actor to the `Worker` actors containing the work assignment
 case class Work(start: Int, nrOfElements: Int) extends PiMessage
-// `Result` -- sent from the `Worker` actors to the `Master` actor containing the result from the worker’s calculation
+// `Result` – sent from the `Worker` actors to the `Master` actor containing the result from the worker’s calculation
 case class Result(value: Double) extends PiMessage
-// `PiApproximation` -- sent from the Master actor to the Listener actor containing the the final pi result and how long
+// `PiApproximation` – sent from the Master actor to the Listener actor containing the the final pi result and how long
 // the calculation took
 case class PiApproximation(pi: Double, duration: Duration) extends PiMessage
 
@@ -30,6 +30,35 @@ class Worker extends Actor {
   }
   def receive = {
     case Work(start, nrOfElements) =>
-      sender ! Result(this.calculatePiFor(start, nrOfElements))
+      sender ! Result(calculatePiFor(start, nrOfElements))
+  }
+}
+
+// `nrOfWorkers` – defining how many workers we should start up
+// `nrOfMessages` – defining how many number chunks to send out to the workers
+// `nrOfElements` – defining how big the number chunks sent to each worker should be
+// Note that we are passing in a `ActorRef` to the `Master` actor. This is used to report the the final result to the
+// outside world.
+class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef) extends Actor {
+  var pi: Double = _
+  var nrOfResults: Int = _
+  val start: Long = System.currentTimeMillis
+  // In the constructor of `Master`, we create a round-robin router to make it easier to spread out the work evenly
+  // between the workers.
+  val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
+
+  def receive = {
+    case Calculate =>
+      for (i <- 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
+    case Result(value) =>
+      pi += value
+      nrOfResults += 1
+      if (nrOfResults == nrOfMessages) {
+        // Send the result to the listener
+        listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
+        // Stops this actor and all its supervised children – In this case it has one supervised actor, the router, and
+        // this in turn has nrOfWorkers supervised actors.
+        context.stop(self)
+      }
   }
 }
